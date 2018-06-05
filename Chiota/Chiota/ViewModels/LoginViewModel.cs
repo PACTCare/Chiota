8471b0a5ef2f1c1e6a802912c786b6e4b56bf5fc
Noc2 <david.hawig@florence.chat>
2018-06-05 22:42:33 +0200

    Seed check back button, don't generate addresses again

--- Chiota/Chiota/ViewModels/LoginViewModel.cs
+++ Chiota/Chiota/ViewModels/LoginViewModel.cs
@@ -31,6 +31,8 @@
 
     private UserDataOnTangle dataOnTangle;
 
+    private User user;
+
     public LoginViewModel()
     {
       this.StoreSeed = true;
@@ -79,48 +81,52 @@
       {
         this.IsBusy = true;
         this.AlreadyClicked = true;
-        var seed = new Seed(this.RandomSeed);
 
-        // 4 addresses needed
-        // 0. own user data address (encrypted, MAM or private key)
-        // 1. public key address 
-        // 2. request address
-        // 3. approved address
-        // addresses can be generated based on each other to make it faster
-        var addressGenerator = await Task.Run(() => new AddressGenerator(new Kerl(), new KeyGenerator(new Kerl(), new IssSigningHelper())));
-        var addresses = await Task.Run(() => addressGenerator.GetAddresses(seed, SecurityLevel.Medium, 0, 2));
+        // Don't generate addresses again, in case user uses the back button to copy the seed and doesn't change it.
+        if (this.user == null || this.RandomSeed != this.user?.Seed.Value)
+        {
+          var seed = new Seed(this.RandomSeed);
 
-        // var addresses = await this.GenerateAddressParallel(seed, 2);
-        addresses.Add(Helper.GenerateAddress(addresses[0]));
-        addresses.Add(Helper.GenerateAddress(addresses[1]));
+          // 0. own user data address (encrypted, MAM or private key)
+          // 1. public key address 
+          // 2. request address
+          // 3. approved address
+          // addresses can be generated based on each other to make it faster
+          var addressGenerator = await Task.Run(() => new AddressGenerator(new Kerl(), new KeyGenerator(new Kerl(), new IssSigningHelper())));
+          var addresses = await Task.Run(() => addressGenerator.GetAddresses(seed, SecurityLevel.Medium, 0, 2));
 
-        var user = new UserFactory().Create(seed, addresses);
-        user = IotaHelper.GenerateKeys(user);
+          // var addresses = await this.GenerateAddressParallel(seed, 2);
+          addresses.Add(Helper.GenerateAddress(addresses[0]));
+          addresses.Add(Helper.GenerateAddress(addresses[1]));
+
+          this.user = new UserFactory().Create(seed, addresses);
+          this.user = IotaHelper.GenerateKeys(this.user);
+        }
 
         // if first time only store seed after finished setup
-        user.StoreSeed = this.StoreSeed;
+        this.user.StoreSeed = this.StoreSeed;
 
-        this.dataOnTangle = new UserDataOnTangle(user);
-        user = await this.dataOnTangle.UpdateUserWithOwnDataAddress();
+        this.dataOnTangle = new UserDataOnTangle(this.user);
+        this.user = await this.dataOnTangle.UpdateUserWithOwnDataAddress();
 
-        if (user.Name == null)
+        if (this.user.Name == null)
         {
           this.IsBusy = false;
           this.AlreadyClicked = false;
-          await this.Navigation.PushModalAsync(new NavigationPage(new CheckSeedStoredPage(user)));
+          await this.Navigation.PushModalAsync(new NavigationPage(new CheckSeedStoredPage(this.user)));
         }
         else
         {
-          user = await this.dataOnTangle.UniquePublicKey();
-          if (user.StoreSeed)
+          this.user = await this.dataOnTangle.UniquePublicKey();
+          if (this.user.StoreSeed)
           {
-            new SecureStorage().StoreUser(user);
+            new SecureStorage().StoreUser(this.user);
           }
 
           this.IsBusy = false;
-          if (user.NtruChatPair != null)
+          if (this.user.NtruChatPair != null)
           {
-            Application.Current.MainPage = new NavigationPage(new ContactPage(user));
+            Application.Current.MainPage = new NavigationPage(new ContactPage(this.user));
             await this.Navigation.PopToRootAsync(true);
           }
           else
