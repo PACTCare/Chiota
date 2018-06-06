6046ab59f1dbb52cbd1c86a84b77a3bf6dc78c15
Felandil <s.fuhrhop@hotmail.de>
2018-06-06 20:18:24 +0200

    fire event with user data on setup completion

---
+++ Chiota/Chiota/Events/SetupEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace Chiota.Events
+{
+  using System;
+
+  using Chiota.Models;
+
+  /// <summary>
+  /// The setup event args.
+  /// </summary>
+  public class SetupEventArgs : EventArgs
+  {
+    /// <summary>
+    /// Gets or sets the user.
+    /// </summary>
+    public User User { get; set; }
+  }
+}
