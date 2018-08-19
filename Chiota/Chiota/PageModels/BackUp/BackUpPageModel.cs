640de45925faa600c9c631ac3d248d0965283783
ThunderSL <SL-Thunder@gmx.de>
2018-08-19 17:00:42 +0200

    Add back up page

---
+++ Chiota/Chiota/PageModels/BackUp/BackUpPageModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Chiota.PageModels.Classes;
+using Xamarin.Forms;
+
+namespace Chiota.PageModels.BackUp
+{
+    public class BackUpPageModel : BasePageModel
+    {
+        #region Attributes
+
+        private bool _isContinueEnabled;
+
+        #endregion
+
+        #region Properties
+
+        public bool IsContinueEnabled
+        {
+            get => _isContinueEnabled;
+            set
+            {
+                _isContinueEnabled = value;
+                OnPropertyChanged(nameof(IsContinueEnabled));
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        #region Help
+
+        public ICommand HelpCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
+
+        #endregion
+
+        #region WriteSeed
+
+        public ICommand WriteSeedCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
+
+        #endregion
+
+        #region PrintPaper
+
+        public ICommand PrintPaperCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
+
+        #endregion
+
+        #region QrCode
+
+        public ICommand QrCodeCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
+
+        #endregion
+
+        #region Continue
+
+        public ICommand ContinueCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+
+                });
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
